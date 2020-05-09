1bf2b086827ab14da17cfee516fd0895bcba8032
Sergei <sergei.konoshenko2000@gmail.com>
2020-05-09 21:41:06 +0300

    Fix Heroku1.2

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
 from app import create_app
 
-app = create_app()
+if __name__ == '__main__':
+    app = create_app()
+    app.run(host=app.config['HOST'], port=app.config['PORT'])
+
+#app = create_app()
