56a14d6b72ce76bc37a8a0b912023b7ec17ea8cb
Sergei <sergei.konoshenko2000@gmail.com>
2020-05-09 17:49:20 +0300

    Fix Procfile2

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from app import create_app
 
-if __name__ == '__main__':
-    app = create_app()
-    app.run(host=app.config['HOST'], port=app.config['PORT'])
+appl = create_app()
+appl.run(host=appl.config['HOST'], port=appl.config['PORT'])
