61d22c166cca2a625a96b31ed7d4b531946d8ee2
Sergei <sergei.konoshenko2000@gmail.com>
2020-05-09 19:20:13 +0300

    Fix Procfile4.6

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ class Config(object):
     DB_PASS = os.getenv('DB_PASS')
     DB_USER = os.getenv('DB_USER')
 
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    #SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     USER_ENABLE_EMAIL = False
     USER_ENABLE_USERNAME = True
